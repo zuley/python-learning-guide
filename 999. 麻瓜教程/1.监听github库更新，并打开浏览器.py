641f506fb5c7f6afbcde57aea256d77649758342
猪不乐意 <914230482@qq.com>
2018-11-06 20:56:43 +0800

    增加示例：监测 github 库更新

--- 999. 麻瓜教程/1.监听github库更新，并打开浏览器.py
+++ 999. 麻瓜教程/1.监听github库更新，并打开浏览器.py
@@ -6,6 +6,7 @@ import webbrowser
 gitUrl = 'https://github.com/zuley/python-learning-guide/commits/master'
 hitDate = '2018-09-09T06:09:00Z'
 
+# 声明获取 github 仓库信息的函数
 def getGithubUpdate () :
   try:
     doc = pq(requests.get(gitUrl).text)
@@ -13,9 +14,11 @@ def getGithubUpdate () :
   except expression as identifier:
     print('获取内容失败', expression)
 
+# 如果是作为入口运行
 if __name__ == '__main__':
+  # 循环
   while (True):
-    print('进入循环，开始获取github仓库形象')
+    print('进入循环，开始获取github仓库信息')
     curDate = getGithubUpdate()
     if curDate > hitDate:
       print('github 仓库已经更新，打开浏览器')
@@ -23,4 +26,5 @@ if __name__ == '__main__':
       hitDate = curDate
     else:
       print('github 仓库未更新，跳过继续下一个循环。')
+    # 延迟三秒运行
     sleep(3)
