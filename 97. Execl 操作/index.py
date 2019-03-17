cf5fe54af103f6017a6263ed91a181442c6203e3
zuley <zhu14leyi1412@gmail.com>
2019-03-17 22:39:57 +0800

    更新处理文件

--- 97. Execl 操作/index.py
+++ 97. Execl 操作/index.py
@@ -1,19 +1,19 @@
 from openpyxl import load_workbook, Workbook
 from openpyxl.utils import get_column_letter, column_index_from_string
 import time
-
+import math
 # 获取文件
-doc = load_workbook('应收历史回款统计表-2.xlsx', data_only=True)
+doc = load_workbook('医疗应收账款字段表.xlsx', data_only=True)
 
 # 计算开始索引和结束索引
 start_index = column_index_from_string('G') - 1
-end_index = column_index_from_string('AH') - 1
+end_index = column_index_from_string('AT') - 1
 
 # 客户名称索引
 name_index = column_index_from_string('D') - 1
 
 # 打开工作表
-a_sheet = doc['历史应收周转表']
+a_sheet = doc['历史新增应收账款表现']
 
 huikuanTitle = [
     '1月内回款',
@@ -28,18 +28,22 @@ huikuanTitle = [
     '10月内回款',
     '11月内回款',
     '12月内回款',
-    '13月内回款'
+    '13月内回款',
+    '14月内回款',
+    '15月内回款',
+    '16月内回款',
+    '17月内回款',
+    '18月内回款',
+    '19月内回款',
+    '20月内回款',
+    '21月内回款',
 ]
 
-# 过滤数据表
-nameFilter = [
-    '国药控股吉林医疗器械有限公司',
-    '龙山县人民医院',
-    '泊头市医院'
-]
+# 过滤数据表，以公司名字过滤
+nameFilter = []
 
 
-# 函数：获取日期列表
+# 函数：获取日期列表 如 2017年5月
 def getDateTitle ():
     sList = list(a_sheet.rows)[1]
     arr = {}
@@ -54,6 +58,7 @@ def getHuikuan (add, reduce, name):
     allAdd = 0
     # 遍历增加，以每一个增加为一行数据
     for index, item in enumerate(add):
+        # 查看公司是否在过滤名单中
         if name in nameFilter: break
         allAdd += item['add']
         # 全部减少数
@@ -62,18 +67,20 @@ def getHuikuan (add, reduce, name):
         status = False
         # 定义数据行
         row = {}
-        # 设置名称
+        # 设置公司名
         row['name'] = name
         # 当前增加的日期
         nowYear = item['date'].year
         nowMonth = item['date'].month
+        # 设置当前月份
         row['date'] = '%s年%s月' % (nowYear, nowMonth)
         # 当期增加
         row['add'] = item['add']
         # 遍历减少，计算回款
-        for rindex, reduceItem in enumerate(reduce):
+        for rindex, reduceItem in enumerate(reduce): 
             allReduce += reduceItem['reduce']
             # 回款日期
+            print(name, reduceItem)
             rYear = reduceItem['date'].year
             rMonth = reduceItem['date'].month
             # 计算回款间隔
@@ -87,10 +94,7 @@ def getHuikuan (add, reduce, name):
                     row[huikuanTitle[n]] = 0
             # 回款差额
             nChae = allReduce - allAdd
-            if name == '北镇市中医院':
-                print('距离几个月', num, item['date'], reduceItem['date'])
-                print('回款差额', round(1 + nChae/item['add'], 6))
-            # 当减少数大于增加数的时候，回款率为1，此循环结束
+            # 当减少数大于增加数的时候，回款率为1，此循 环结束
             if nChae >= 0:
                 row[huikuanTitle[num]] = 1
                 break
@@ -98,31 +102,30 @@ def getHuikuan (add, reduce, name):
                 nHuikuanlv = 0
                 row[huikuanTitle[num]] = nHuikuanlv
             else:
-                nHuikuanlv = round(1 + nChae/item['add'], 6)
+                # 保留6位小数
+                nHuikuanlv = math.floor((1 + nChae/item['add']) * 1000000) / 1000000
                 row[huikuanTitle[num]] = nHuikuanlv
         data.append(row)
-        if name == '北镇市中医院':
-            print('北镇市中医院')
-            for item in add: print(item)
-            print('-')
-            for item in reduce: print(item)
-            print(row)
     return data
 
-
+# 获取表格的日期列表
 dateTitle = getDateTitle()
+# 新的数据表数据
 newSheet = []
 
-for row in list(a_sheet.rows)[3:137]:
+# 遍历源数据表
+for row in list(a_sheet.rows)[3:312]:
     # 客户名称
     name = row[name_index].value
     # 存储本期增加的资金列表
     nowDateAdd = []
+    # 存储本期减少的资金列表
     nowDateReduceNum = []
-    # 遍历行
+    # 遍历当前行中的资金数据
     for index in range(start_index, end_index, 2):
         # 取得本期增加
         addNum = row[index].value
+        # 如果本期增加的数据是字符 - 或者没有数据，则设置为 0
         if addNum == '-' or not addNum:
             addNum = 0
         # 如果大于0则组合数据
@@ -133,8 +136,10 @@ for row in list(a_sheet.rows)[3:137]:
             })
         # 取得本期减少
         reduceNum = row[index + 1].value
+        # 如果本期减少的数据是字符 - 或者没有数据，则设置为 0
         if reduceNum == '-' or not reduceNum:
             reduceNum = 0
+        # 如果大于0则组合数据
         if reduceNum > 0:
             nowDateReduceNum.append({
                 'date': dateTitle[index],
@@ -143,6 +148,7 @@ for row in list(a_sheet.rows)[3:137]:
     # 计算回款
     newSheet += getHuikuan(nowDateAdd, nowDateReduceNum, name)
 
+# 创建新的表格
 newXlsx = Workbook()
 newXlsxS1 = newXlsx.active
 newXlsxS1.title = '回款率统计表'
@@ -152,15 +158,17 @@ newXlsxS1['A1'] = '客户名称'
 newXlsxS1['B1'] = '时间'
 newXlsxS1['C1'] = '新增'
 
+# 写表头，写入汇款日期表头
 for index, item in enumerate(huikuanTitle):
     newXlsxS1.cell(row=1, column=index+4, value=item)
 
-# 写数据
+# 写数据，遍历回款数据数组写入数据
 for index, row in enumerate(newSheet):
-    # print(row)
     for j, column in enumerate(row):
+        # 从第二行开始写入
         newXlsxS1.cell(row=index+2, column=j+1, value=row[column])
 
+# 保存文件
 newXlsx.save(filename='回款率统计表.xlsx')
 
 
